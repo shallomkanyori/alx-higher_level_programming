0bb69bf04a2818a9eee84ea596217529945fd999
Shallom Kanyori <shallomkanyori@gmail.com>
2023-06-14 08:29:08 +0000

    Add Python function that replaces all occurrences of element in list
    Add a Python function that replaces all occurrences of an element by
    another in a new list.

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    """Replaces all occurences of element by another in a new list."""
+    if my_list:
+        return [(replace if el == search else el) for el in my_list]

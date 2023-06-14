f3a0d0d8b61f775181dcf560641e60cb03c1755c
Shallom Kanyori <shallomkanyori@gmail.com>
2023-06-14 09:10:03 +0000

    Modify code to check that list is not None specifically

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     """Replaces all occurences of element by another in a new list."""
-    if my_list:
+    if my_list is not None:
         return [(replace if el == search else el) for el in my_list]
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     """Returns the sum of all unique integers in a list."""
-    if my_list:
+    if my_list is not None:
         uniq_sum = 0
         for x in set(my_list):
             uniq_sum += x

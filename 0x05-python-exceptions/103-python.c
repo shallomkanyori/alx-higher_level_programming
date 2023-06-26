df05b7ce466609f5c79bd2be690abcc82b423137
Shallom Kanyori <shallomkanyori@gmail.com>
2023-06-26 20:00:14 +0000

    Add C functions that print basic info on Python lists, bytes and floats

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,118 @@
+#include "Python.h"
+
+/**
+ * print_python_float - prints some basic info about a Python float object
+ * @p: a pointer to the PyObject that is the Python float object
+ *
+ * Return: nothing.
+ */
+void print_python_float(PyObject *p)
+{
+	double value;
+	char *v_str;
+
+	printf("[.] float object info\n");
+
+	if (!PyFloat_Check(p))
+	{
+		printf("  [ERROR] Invalid Float Object\n");
+		fflush(stdout);
+		return;
+	}
+
+	value = PyFloat_AsDouble(p);
+	v_str = PyOS_double_to_string(value, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
+
+	printf("  value: %s\n", v_str);
+	fflush(stdout);
+
+	PyMem_Free((void *)v_str);
+}
+
+/**
+ * print_python_bytes - prints some basic info about Python bytes
+ * @p: a pointer to the PyObject that is the Python bytes
+ *
+ * Return: nothing.
+ */
+void print_python_bytes(PyObject *p)
+{
+	Py_ssize_t size;
+	Py_ssize_t peek_size;
+	Py_ssize_t ind;
+	char *byte_str;
+
+	printf("[.] bytes object info\n");
+
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		fflush(stdout);
+		return;
+	}
+
+	size = PyBytes_Size(p);
+	peek_size = size > 10 ? 10 : (size + 1);
+	byte_str = ((PyBytesObject *)p)->ob_sval;
+
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", byte_str);
+	printf("  first %ld bytes: ", peek_size);
+
+	for (ind = 0; ind < peek_size; ind++)
+	{
+		printf("%02x", byte_str[ind] & 0xff);
+
+		if (ind == (peek_size - 1))
+			printf("\n");
+		else
+			printf(" ");
+	}
+
+	fflush(stdout);
+}
+
+/**
+ * print_python_list - prints some basic info about Python lists
+ * @p: a pointer to the PyObject that is the list
+ *
+ * Return: nothing.
+ */
+void print_python_list(PyObject *p)
+{
+	Py_ssize_t list_len;
+	Py_ssize_t allocated;
+	PyObject *item;
+	Py_ssize_t ind;
+	const char *type;
+
+	printf("[*] Python list info\n");
+
+	if (!PyList_Check(p))
+	{
+		printf("  [ERROR] Invalid List Object\n");
+		fflush(stdout);
+		return;
+	}
+
+	list_len = PyList_GET_SIZE(p);
+	allocated = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %ld\n", list_len);
+	printf("[*] Allocated = %ld\n", allocated);
+
+	for (ind = 0; ind < list_len; ind++)
+	{
+		item = PyList_GET_ITEM(p, ind);
+		type = item->ob_type->tp_name;
+
+		printf("Element %ld: %s\n", ind, type);
+
+		if (strcmp(type, "bytes") == 0)
+			print_python_bytes(item);
+		else if (strcmp(type, "float") == 0)
+			print_python_float(item);
+	}
+
+	fflush(stdout);
+}
--- 0x05-python-exceptions/README.md
+++ 0x05-python-exceptions/README.md
@@ -90,3 +90,42 @@ Write a function that executes a function safely.
 
 **Task ByteCode -> Python #4.**
 Write the Python function `def magic_calculation(a, b):` that does exactly the same as the given Python bytecode.
+
+**Task 10: CPython #2: PyFloatObject.**
+Create three C functions that print some basic info about Python lists, Python bytes and Python float objects.
+Python lists:
+- Prototype: `void print_python_list(PyObject *p);`
+- If `p` is not a valid `PyListObject`, print an error message
+
+Python bytes:
+- Prototype: `void print_python_bytes(PyObject *p);`
+- Line “first X bytes”: print a maximum of 10 bytes
+- If `p` is not a valid `PyBytesObject`, print an error message
+
+Python float:
+- Prototype: `void print_python_float(PyObject *p);`
+- If `p` is not a valid `PyFloatObject`, print an error message
+
+About:
+- Python version: 3.4
+- You are allowed to use the C standard library
+- Your shared library will be compiled with this command line: `gcc -Wall -Werror -Wextra -pedantic -std=c99 -shared -Wl,-soname,libPython.so -o libPython.so -fPIC -I/usr/include/python3.4 103-python.c`
+- You are not allowed to use the following macros/functions:
+	- `Py_SIZE`
+	- `Py_TYPE`
+	- `PyList_Size`
+	- `PyList_GetItem`
+	- `PyBytes_AS_STRING`
+	- `PyBytes_GET_SIZE`
+	- `PyBytes_AsString`
+	- `PyBytes_AsStringAndSize`
+	- `PyFloat_AS_DOUBLE`
+	- `PySequence_GetItem`
+	- `PySequence_Fast_GET_SIZE`
+	- `PySequence_Fast_GET_ITEM`
+	- `PySequence_ITEM`
+	- `PySequence_Fast_ITEMS`
+
+**NOTE:**
+- The python script will be launched using the `-u` option (Force `stdout` to be unbuffered).
+- It is strongly advised to either use `setbuf(stdout, NULL);` or `fflush(stdout)` in your C functions IF you choose to use printf. The reason to that is that Python's `print` and `libC`'s `printf` don’t share the same buffer, and the output can appear disordered.

3bfcf8dce733b16057fe6670a0c2637b48aba36e
Shallom Kanyori <shallomkanyori@gmail.com>
2023-06-27 17:09:54 +0000

    Add to __init__ docstring

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,8 +5,13 @@ import math
 
 class MagicClass():
     """Defines a circle"""
+
     def __init__(self, radius):
-        """Initializes the circle"""
+        """Initializes the circle.
+
+        Args:
+            radius (int or float): the radius of the circle."""
+
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
@@ -14,8 +19,8 @@ class MagicClass():
 
     def area(self):
         """Returns the area of the circle"""
-        return self.__radius ** 2 * math.pi
+        return (self.__radius ** 2 * math.pi)
 
     def circumference(self):
         """Returns the circumference of the circle"""
-        return 2 * math.pi * self.__radius
+        return (2 * math.pi * self.__radius)

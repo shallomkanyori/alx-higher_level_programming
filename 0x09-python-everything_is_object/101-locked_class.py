39a223324dada373ba52d9f6f94067de0ead3f95
Shallom Kanyori <shallomkanyori@gmail.com>
2023-07-04 12:45:48 +0000

    Change implementation to use __slots__

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,21 +2,11 @@
 """This module defines the LockedClass class."""
 
 
-class LockedClass(object):
+class LockedClass():
     """This class prevents dynamic creation of most instance attributes.
 
     It prevents the dynamic creation of attributes that are not called
     first_name.
     """
 
-    def __init__(self):
-        """Does nothing."""
-        pass
-
-    def __setattr__(self, key, value):
-        if key != "first_name":
-            err_msg = "'{}' object has no attribute '{}'".format(
-                    type(self).__name__, key)
-            raise AttributeError(err_msg)
-
-        object.__setattr__(self, key, value)
+    __slots__ = "first_name"

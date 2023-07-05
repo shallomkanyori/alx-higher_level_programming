d65f01f18349ecfab0b47a601b9faa13a6895b83
Shallom Kanyori <shallomkanyori@gmail.com>
2023-07-05 11:11:17 +0000

    Add tests for max at beginning and one element list

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -11,9 +11,10 @@ class TestMaxInteger(unittest.TestCase):
         """Tests max integer for sequence types."""
         self.assertEqual(max_integer([1, 2, 2.5, 3, 4]), 4)
         self.assertEqual(max_integer((-4, 14, 20, 88, -90)), 88)
+        self.assertEqual(max_integer([10], 10))
         self.assertEqual(max_integer("hello"), 'o')
         self.assertEqual(max_integer(b"hello"), 111)
-        self.assertEqual(max_integer(range(-10, 0, 2)), -2)
+        self.assertEqual(max_integer(range(0, -10, -2)), 0)
         self.assertEqual(max_integer([]), None)
         self.assertEqual(max_integer(), None)
 
@@ -27,8 +28,8 @@ class TestMaxInteger(unittest.TestCase):
     def test_mixed_sequences(self):
         """"Make sure type errors are raised for sequences with mixed types.
 
-            Type errors should be raised for sequences that contain non-numeric
-            mixed types."""
+                Type errors should be raised for sequences that contain
+                non-numeric mixed types."""
         self.assertRaises(TypeError, max_integer, [1, "2", 3, 4])
         self.assertRaises(TypeError, max_integer, ((1, 2), -10, 31))
         self.assertRaises(TypeError, max_integer, [1, 2, (3, 4)])

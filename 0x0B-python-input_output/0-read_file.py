6d07eaa224b5602043353e4b725f2a0e818d48d3
Shallom Kanyori <shallomkanyori@gmail.com>
2023-07-11 07:32:02 +0000

    Add Python function that prints a text file to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module defines the read_file function."""
+
+
+def read_file(filename=""):
+    """Reads text from a file and prints it to stdout.
+
+        Args:
+            filename (string): the name of the file.
+    """
+    with open(filename, encoding="utf_8") as f:
+        print(f.read())

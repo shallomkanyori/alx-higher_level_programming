aaef2794ce13592ebb1830118e3cc5646dd5d1a9
Shallom Kanyori <shallomkanyori@gmail.com>
2023-07-11 14:40:23 +0000

    Restructure code

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
-"""This module is a script that reads stdin and computes metrics."""
+"""This module is a script that reads from stdin and computes metrics."""
 import sys
 
 
-def print_info(total_size, codes):
-    """Prints metrics.
-
-        Args:
-            total_size (int): the total size of all files so far.
-            codes (dict): the frequency count of the status codes.
-    """
-    print(f"File size: {total_size}")
-    [print(f"{k}: {codes[k]}") for k in sorted(codes.keys()) if codes[k] != 0]
-
-
-def main():
+if __name__ == "__main__":
     lines = 0
     total_size = 0
     codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
@@ -28,12 +17,12 @@ def main():
 
             lines += 1
             if lines % 10 == 0:
-                print_info(total_size, codes)
+                print(f"File size: {total_size}")
+                [print(f"{k}: {codes[k]}") for k in sorted(codes.keys())
+                    if codes[k] != 0]
 
     except KeyboardInterrupt:
-        print_info(total_size, codes)
+        print(f"File size: {total_size}")
+        [print(f"{k}: {codes[k]}") for k in sorted(codes.keys())
+            if codes[k] != 0]
         raise
-
-
-if __name__ == "__main__":
-    main()

e56fecdfc7b19189da402f77928948bfeb61a48e
Shallom Kanyori <shallomkanyori@gmail.com>
2023-07-18 19:30:01 +0000

    Add save_to_file with None test for Square

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -194,6 +194,10 @@ class TestBase_save_to_file(unittest.TestCase):
         with open("Rectangle.json", "r") as f:
             self.assertEqual(f.read(), "[]")
 
+        Square.save_to_file(None)
+        with open("Square.json", "r") as f:
+            self.assertEqual(f.read(), "[]")
+
     def test_save_to_file_empty(self):
         """Test the save_to_file method with an empty list."""
         Square.save_to_file([])

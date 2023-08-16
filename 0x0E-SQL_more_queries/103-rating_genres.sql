173e110ed16a47e243ec4336dc5dfeac3af9d01f
Shallom Kanyori <shallomkanyori@gmail.com>
2023-08-16 06:58:16 -0700

    Fix typo in database name

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -3,7 +3,7 @@ SELECT tg.name, SUM(tsr.rate) AS rating
   FROM tv_genres AS tg
        LEFT JOIN tv_show_genres AS tsg
        ON tg.id = tsg.genre_id
-       LEFT JOIN tv_show_rating AS tsr
+       LEFT JOIN tv_show_ratings AS tsr
        ON tsg.show_id = tsr.show_id
  GROUP BY tg.name
  ORDER BY rating DESC;

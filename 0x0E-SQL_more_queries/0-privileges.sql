9709b4699bc6201de9bba386e5fde90c97c81364
Shallom Kanyori <shallomkanyori@gmail.com>
2023-08-16 09:32:38 +0000

    Revert "Remove host in user"
    This reverts commit dc306071177b0eda589c540d96b16abb303a082e.

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Lists all privileges for the users user_0d_1 and user_0d_2 on the server
 -- in localhost
-SHOW GRANTS FOR 'user_0d_1';
-SHOW GRANTS FOR 'user_0d_2';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

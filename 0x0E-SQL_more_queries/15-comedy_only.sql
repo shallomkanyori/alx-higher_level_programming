860e320d90a05aae5b8881af72500bd601d8e0d6
Shallom Kanyori <shallomkanyori@gmail.com>
2023-08-16 05:14:13 -0700

    Fix typo in table name

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 -- Lists all comedy shows in the database hbtn_0d_tvshows
 SELECT ts.title
   FROM tv_shows AS ts
-       INNER JOIN tv_shows_genres AS tsg
+       INNER JOIN tv_show_genres AS tsg
        ON ts.id = tsg.show_id
        INNER JOIN tv_genres AS tg
        ON tsg.genre_id = tg.id

978d5f73f5e6e023f5d33646c74daf21ab80ced8
Shallom Kanyori <shallomkanyori@gmail.com>
2023-08-16 04:58:54 -0700

    Use inner join instead of subquery

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,8 +3,7 @@ SELECT tg.name
   FROM tv_genres AS tg
        INNER JOIN tv_show_genres AS tsg
        ON tg.id = tsg.genre_id
-       WHERE tsg.show_id =
-             (SELECT ts.id
-	        FROM tv_shows AS ts
-	       WHERE ts.title = 'Dexter')
+       INNER JOIN tv_shows as ts
+       ON tsg.show_id = ts.id
+       WHERE ts.title = 'Dexter'
  ORDER BY tg.name;

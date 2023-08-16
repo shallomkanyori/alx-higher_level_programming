17535ef7965da003aed7cd488bf1ba9123c1c56e
Shallom Kanyori <shallomkanyori@gmail.com>
2023-08-16 05:59:33 -0700

    Fix typo in column name

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,7 +2,7 @@
 SELECT ts.title
   FROM tv_shows AS ts
  WHERE ts.id NOT IN
-       (SELECT tvg.show_id
+       (SELECT tsg.show_id
 	  FROM tv_show_genres AS tsg
 	       INNER JOIN tv_genres AS tg
 	       ON tsg.genre_id = tg.id

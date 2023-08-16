863f2fd0791c4d19f718e34c434c74c25b4cddd3
Shallom Kanyori <shallomkanyori@gmail.com>
2023-08-16 05:01:08 -0700

    Rewrite statements in all caps

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,7 +3,7 @@ SELECT tg.name
   FROM tv_genres AS tg
        INNER JOIN tv_show_genres AS tsg
        ON tg.id = tsg.genre_id
-       INNER JOIN tv_shows as ts
+       INNER JOIN tv_shows AS ts
        ON tsg.show_id = ts.id
        WHERE ts.title = 'Dexter'
  ORDER BY tg.name;

9d39df796a76bdc426e59689f202785499af6550
Shallom Kanyori <shallomkanyori@gmail.com>
2023-09-14 09:34:59 +0000

    Add Python script that lists all states from db
    Add a Python script that lists all records in the states table from a
    given database ordered by id (ascending).

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Lists all states from a database."""
+import MySQLdb
+import sys
+
+
+def main():
+    """Lists all states from a given database."""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=db_name, charset="utf8")
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
--- 0x0F-python-object_relational_mapping/README.md
+++ 0x0F-python-object_relational_mapping/README.md
@@ -1,1 +1,9 @@
 ## Python - Object-relational mapping
+
+#### Task 0
+[0-select_states.py](0-select_states.py) is a script that lists all `states` from a given database.
+- Takes 3 arguments: `mysql username`, `mysql password` and `database name` (no argument validation performed)
+- Uses the module `MySQLdb`
+- Connects to a MySQL server running on `localhost` at port `3306`
+- Results are sorted in ascending order by `states.id`
+- Results are displayed one row per line

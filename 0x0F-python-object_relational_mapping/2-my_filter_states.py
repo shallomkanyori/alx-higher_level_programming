8fd31d2b13cd8e613bbad1dcfa08fd32319966d8
Shallom Kanyori <shallomkanyori@gmail.com>
2023-09-14 10:11:13 +0000

    Add Python script that lists filtered states
    Add a Python script that lists all record of the states table from a
    given database, filtered by name according to a given argument.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Lists all states filtered by name from a database."""
+import MySQLdb
+import sys
+
+
+def main():
+    """Lists all states filtered by a given name from a given database."""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=db_name, charset="utf8")
+    cur = conn.cursor()
+
+    query = "SELECT * FROM {} WHERE name = '{}' ORDER BY {} ASC".format(
+            'states',
+            state_name,
+            'id')
+
+    cur.execute(query)
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
--- 0x0F-python-object_relational_mapping/README.md
+++ 0x0F-python-object_relational_mapping/README.md
@@ -15,3 +15,11 @@
 - Connects to a MySQL server running on `localhost` at port `3306`
 - Results are sorted in ascending order by `states.id`
 - Results are displayed one row per line
+
+#### Task 2
+[2-my_filter_states.py](2-my_filter_states.py) is a script that takes in an argument and displays all values in the `states` table of a given database where `name` matches the argument.
+- Takes 4 arguments: `mysql username`, `mysql password`, `database name` and `state name searched` (no argument validation performed)
+- Uses the module `MySQLdb`
+- Connects to a MySQL server running on `localhost` at port `3306`
+- Results are sorted in ascending order by `states.id`
+- Results are displayed one row per line

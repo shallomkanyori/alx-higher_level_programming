7123719d4919bc9765e0d16c796a736b98177b91
Shallom Kanyori <shallomkanyori@gmail.com>
2023-09-28 11:56:57 +0000

    Allow for body without ending newline

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays the response body of a GET request to a given URL
-out=$(curl -s -w "%{http_code}" "$1"); [ "${out: -3}" -eq 200 ] && echo "$out" | sed '$d'
+out=$(curl -sL -w "%{http_code}" "$1"); [ "${out: -3}" -eq 200 ] && printf %s "$out" | sed -E '$ s/.{3}$//'

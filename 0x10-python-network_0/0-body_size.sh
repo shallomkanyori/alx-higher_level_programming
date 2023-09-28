216ea6982b62f3315dc91455cade624d04a094c7
Shallom Kanyori <shallomkanyori@gmail.com>
2023-09-28 09:15:22 +0000

    Restrict script to 3 lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/bash
 # Displays the body size of the response from a request to a given URL
-if [ $# -ne 0 ]; then
-	curl -sI "$1" | grep 'Content-Length:' | sed 's/\r//g' | cut -d " " -f 2
-fi
+curl -sI "$1" | grep 'Content-Length:' | sed 's/\r//g' | cut -d " " -f 2

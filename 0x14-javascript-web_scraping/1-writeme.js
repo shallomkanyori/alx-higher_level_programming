47f3d9ecc2dba85c9e691d6cc61c3bbb26e94d70
Shallom Kanyori <shallomkanyori@gmail.com>
2023-10-24 09:50:34 +0000

    Remove unnecessary trailing newline

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,10 +3,9 @@
 const fs = require('fs');
 
 const filepath = process.argv[2];
-let string = process.argv[3];
+const string = process.argv[3];
 
 try {
-  string = string + '\n';
   fs.writeFileSync(filepath, string, 'utf-8');
 } catch (error) {
   console.log(error);

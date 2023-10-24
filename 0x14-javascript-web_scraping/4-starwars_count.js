2b89b8bb365372294964e2427ab8b900117a98a9
Shallom Kanyori <shallomkanyori@gmail.com>
2023-10-24 13:34:04 +0000

    Use different method to count films

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,13 +3,12 @@
 const request = require('request');
 
 const apiUrl = process.argv[2];
-const personUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
 
 request(apiUrl, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    const count = body.split(personUrl).length - 1;
+    const count = body.split('/people/18/').length - 1;
     console.log(count);
   }
 });

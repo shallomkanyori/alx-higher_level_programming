5ed5c8cc11e921d9adeab96179b879ae38fa2158
Shallom Kanyori <shallomkanyori@gmail.com>
2023-11-01 08:34:08 +0000

    Use correct function name to get input value

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,6 +1,6 @@
 $(function () {
   $('INPUT#btn_translate').on('click', function (event) {
-    const langCode = $('INPUT#language_code').value();
+    const langCode = $('INPUT#language_code').val();
 
     $.get('https://hellosalut.stefanbohacek.dev/', { lang: langCode }, function (response) {
       $('DIV#hello').text(response.hello);
